c073e3a2e196db0af97d43cbc2856af4a14ef938
Edidiong Ephraim <ephraimedy@gmail.com>
2023-11-08 14:52:07 +0100

    wrote part of deploy script and its error handling

---
+++ ethers-simplestorage/deploy.js
@@ -0,0 +1,14 @@
+// Smart contract deployment script
+
+// Deploy function
+async function main() {
+    
+}
+
+// This checks if the async function has executed properly, if not it returns the error message
+main()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    })
